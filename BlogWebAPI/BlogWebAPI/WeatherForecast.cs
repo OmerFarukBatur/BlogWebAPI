eeff9ad34ab2fe002e0dd09e83e7e0f64e883fa7
OmerFarukBatur <2506232@ogrenci.bilecik.edu.tr>
2022-03-21 14:17:03 +0300

    deneme

--- BlogWebAPI/BlogWebAPI/WeatherForecast.cs
+++ BlogWebAPI/BlogWebAPI/WeatherForecast.cs
@@ -9,5 +9,8 @@ namespace BlogWebAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string? Summary1 { get; set; }
+        public string? Summary2 { get; set; }
+        // denememme
     }
 }
