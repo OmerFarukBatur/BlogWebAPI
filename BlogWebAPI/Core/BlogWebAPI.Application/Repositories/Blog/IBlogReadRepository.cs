dbe9b7f0d23ea9277866c553cc154467c7701e18
OmerFarukBatur <2506232@ogrenci.bilecik.edu.tr>
2022-03-24 12:02:43 +0300

    repository

---
+++ BlogWebAPI/Core/BlogWebAPI.Application/Repositories/Blog/IBlogReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlogWebAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogWebAPI.Application.Repositories
+{
+    public interface IBlogReadRepository : IReadRepository<Blog>
+    {
+    }
+}
